b56847ae471604b63f87a41deb4239e55f0d6145
Tellinq <59412384+Tellinq@users.noreply.github.com>
2025-09-18 21:50:32 -0400

    Add 1.21.6-1.21.8 support

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,7 +23,7 @@ pluginManagement {
     }
 
     plugins {
-        kotlin("jvm") version("2.0.20")
+        kotlin("jvm") version("2.2.10")
         id("dev.deftu.gradle.multiversion-root") version("2.+")
     }
 }
@@ -84,7 +84,16 @@ listOf(
     "1.21.4-fabric",
 
     "1.21.5-neoforge",
-    "1.21.5-fabric"
+    "1.21.5-fabric",
+
+    "1.21.6-neoforge",
+    "1.21.6-fabric",
+
+    "1.21.7-neoforge",
+    "1.21.7-fabric",
+
+    "1.21.8-neoforge",
+    "1.21.8-fabric",
 ).forEach { version ->
     include(":$version")
     project(":$version").apply {
